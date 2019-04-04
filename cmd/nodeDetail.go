683e8364f71523d5acfbea9044c62b9ac6626ef4
Nikhil Chawla <nichawla@redhat.com>
2019-04-04 16:33:53 +0530

    Added copyrights changes

--- cmd/nodeDetail.go
+++ cmd/nodeDetail.go
@@ -1,4 +1,4 @@
-// Copyright © 2019 NAME HERE chawlanikhil24@gmail.com
+// Copyright © 2019 Nikhli Chawla chawlanikhil24@gmail.com
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
